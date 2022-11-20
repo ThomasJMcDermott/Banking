3f269a1ed9defa593ae028893f589fc5e6c5022f
Thomas J. McDermott <118658340+ThomasJMcDermott@users.noreply.github.com>
2022-11-21 08:59:18 +1100

    Add files via upload

---
+++ Models/Addresses.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace s3851480_a3.Models;
+
+public class Addresses  
+{
+
+    public int AddressesID { get; set; }
+    public int ContactID { get; set; }
+    public virtual Contacts Contact { get; set; }
+
+    [StringLength(100)]
+    public string Street { get; set; }
+    
+    [StringLength(100), RegularExpression(@"^[A-Z]+[a-zA-Z ]*$", ErrorMessage = "Invalid format, Must start with an upper-case letter and only contain letters.")]
+    public string Suburb { get; set; }
+
+    [RegularExpression(@"^\d{4}$")]
+    public string PostCode { get; set; }
+
+    [StringLength(3), RegularExpression(@"\b(ACT|NSW|NT|QLD|SA|TAS|VIC|WA)\b", ErrorMessage = "Must be a valid Australian State")]
+    public string State { get; set; }
+
+}
---
+++ Models/Contacts.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace s3851480_a3.Models;
+
+public class Contacts
+{
+    public int ContactID { get; set; }
+
+    [Display(Name = "First name")]
+    [StringLength(30), RegularExpression(@"^[A-Z]+[a-zA-Z]*$", ErrorMessage = "Invalid format, Must start with an upper-case letter and only contain letters.")]
+    public string FirstName { get; set; }
+
+    [Display(Name = "Last name")]
+    [StringLength(30), RegularExpression(@"^[A-Z]+[a-zA-Z]*$", ErrorMessage = "Invalid format, Must start with an upper-case letter and only contain letters.")]
+    public string LastName { get; set; }
+
+    [Display(Name = "Email address")]
+    [Required(ErrorMessage = "The email address is required")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; set; }
+
+    [Display(Name = "Mobile Phone")]
+    [StringLength(10), RegularExpression(@"^04\d{2}\d{3}\d{3}$")]
+    public string? Mobile { get; set; }
+    public virtual Addresses? HomeAddressID { get; set; }
+    public virtual Addresses? WorkAddressID { get; set; }
+
+}
